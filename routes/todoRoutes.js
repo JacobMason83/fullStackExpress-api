c2abbb85a3421b1bc7ad82255eeb4862dec68bbf
JacobMason83 <jmason062083@gmail.com>
2021-01-08 13:37:22 -0500

    changed the id of the post route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,12 +1,31 @@
 const { response } = require("express")
 const express = require("express")
 const router = express.Router()
-const todos = require("../models/todoModel")
+const Todos = require("../models/todoModel")
 
 //get route
 router.get("/todos", (request, res) => {
-    console.log("We out here")
-    res.end()
+    Todos.find(() => (err, todos) => {
+        if(err) {
+            res.status(404).json({message: "Get todos err", error: `${err}`})
+        } else {
+           res.status(200).json(todos) 
+        }
+    })
+    
+})
+router.post('/todo', (req, res) => {
+    const todo = new Todos(req.body)
+
+    todo 
+      .save()
+      .then(todo => {          
+          const {_id, title, done} = todo          
+          res.status(200).json({ id: _id, title, done})
+      })
+      .catch(err => {
+          res.status(400).json({ message: "unable to post", errors: `${err}`})
+      })
 })
 
 
