1e75ec94768fdbde1556c18d6069ab7194e398fc
JacobMason83 <jmason062083@gmail.com>
2021-01-08 13:50:29 -0500

    added post route and id change to the code

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,32 +1,32 @@
-const { response } = require("express")
-const express = require("express")
-const router = express.Router()
-const Todos = require("../models/todoModel")
-
-//get route
-router.get("/todos", (request, res) => {
-    Todos.find(() => (err, todos) => {
-        if(err) {
-            res.status(404).json({message: "Get todos err", error: `${err}`})
-        } else {
-           res.status(200).json(todos) 
-        }
-    })
-    
-})
-router.post('/todo', (req, res) => {
-    const todo = new Todos(req.body)
-
-    todo 
-      .save()
-      .then(todo => {          
-          const {_id, title, done} = todo          
-          res.status(200).json({ id: _id, title, done})
-      })
-      .catch(err => {
-          res.status(400).json({ message: "unable to post", errors: `${err}`})
-      })
-})
-
-
-module.exports = router
+const express = require("express");
+const router = express.Router();
+const Todos = require("../models/todoModel");
+// GET
+router.get("/todos", (req, res) => {
+	Todos.find((err, todos) => {
+		if (err) {
+			res.status(404).json({ message: "Get todos err", error: `${err}` });
+		} else {
+			res.status(200).json([
+				...todos.map((todo) => {
+					const { _id, title, done } = todo;
+					return { id: _id, title, done };
+				}),
+			]);
+		}
+	});
+});
+// POST
+router.post("/todo", (req, res) => {
+	const todo = new Todos(req.body);
+	todo
+		.save()
+		.then((todo) => {
+			const { _id, title, done } = todo;
+			res.status(200).json({ id: _id, title, done });
+		})
+		.catch((err) => {
+			res.status(400).json({ message: "unable to post", errors: `${err}` });
+		});
+});
+module.exports = router;
