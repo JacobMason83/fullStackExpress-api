6643914275a91d32a539bfced2acb6af9a20232a
JacobMason83 <jmason062083@gmail.com>
2021-01-08 11:51:27 -0500

    adds connection to the db

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const cors = require('cors')
 
-const port = process.env.Port ||4000;
+const port = process.env.Port || 4000;
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/todo_db", err => {
+    if(err) {
+        console.error("Mongo Connection Error", err)
+    } else {
+        console.log('Database Connected')
+    }
+})
+
+app.use(cors())
+app.use(express.json())
 
 app.listen(port, ()=> {
     console.log(`server is up on port ${port}`)
